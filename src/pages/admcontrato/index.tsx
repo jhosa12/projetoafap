05b54ce4a2b2e038153aea801a5116e21ba58421
HenrickFB <61285599+jhosa12@users.noreply.github.com>
2024-01-24 13:54:10 -0300

    v1

--- src/pages/admcontrato/index.tsx
+++ src/pages/admcontrato/index.tsx
@@ -33,20 +33,16 @@ export default function AdmContrato(){
           }
     }
   useEffect(() => {
-   
     const carregarDadosAsync = async () => {
       try {
         await carregarDados();
-      
       } catch (error) {
         console.error('Erro ao carregar dados:', error);
       }
     };
-    
     carregarDadosAsync();
   }, [data.id_associado,data.mensalidade]);
 
-  
   useEffect(() => {
     let x = 0;
       if (dadosassociado?.contrato.situacao === 'INATIVO') {
@@ -160,10 +156,10 @@ export default function AdmContrato(){
         {historico && (
            
 <div   className="flex flex-col rounded-lg  max-h-[calc(100vh-200px)]  p-2 shadow-md sm:rounded-lg">
-<label className="relative inline-flex items-center mb-5 cursor-pointer">
+<label className="relative inline-flex w-[130px] items-center mb-5 cursor-pointer">
   <input onChange={()=>setCheck(!checkMensal)} type="checkbox" value="2" className="sr-only peer"/>
   <div className="w-9 h-5  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-800 rounded-full peer bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all border-gray-600 peer-checked:bg-blue-600"></div>
-  <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Exibir Todas</span>
+  <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Exibir Pagas</span>
 </label>
     <table 
     ref={tabelaRef}
