518315b684096b056c4692ae93d26f3260fc7b1b
HenrickFB <61285599+jhosa12@users.noreply.github.com>
2024-03-10 11:27:22 -0300

    v2

--- src/pages/gerenciarAdministrativo/index.tsx
+++ src/pages/gerenciarAdministrativo/index.tsx
@@ -1,12 +1,56 @@
 import { MenuLateral } from "@/components/menu"
+import { api } from "@/services/apiClient"
 import Head from "next/head"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+
+
+interface PlanoContas{
+    conta: string,
+    id_grupo: number,
+    descricao: string,
+    tipo: string,
+    saldo: number,
+    perm_lanc: string,
+    data: Date,
+    hora: Date,
+    usuario: string,
+    contaf: string
+}
+interface GruposProps{
+    id_grupo:number,
+    descricao:string
+}
+interface DadosProps{
+    plano_contas:Array<PlanoContas>
+    grupos:Array<GruposProps>
+ 
+}
 
 
 export default function gerenciarAdministrativo(){
     const [PlanosContas,setPlanosContas] =useState(true)
     const [Planos,setPlanos] = useState(false)
     const [Convalescencia,setConv] =useState(false)
+    const [dados,setDados] = useState<DadosProps>()
+
+useEffect(()=>{
+    try{
+carregarDados()
+      
+    }catch(err){
+        console.log(err)
+
+    }
+},[])
+
+async function carregarDados() {
+    const response= await api.get('/gerenciarAdministrativo')
+    setDados(response.data)
+    console.log(response.data)
+    
+}
+
     return(
 <>
 <Head>
@@ -27,7 +71,104 @@ export default function gerenciarAdministrativo(){
         </li>
 
     </ul>
+    <div className="inline-flex">
+    <div className="flex flex-col rounded-lg  max-h-[calc(100vh-200px)] p-2 max-w-[550px]   shadow-md sm:rounded-lg">
+    <table 
+     className="block  overflow-y-auto overflow-x-auto text-sm text-left rtl:text-center border-collapse rounded-lg text-gray-400">
+        <thead className="sticky top-0  text-xs uppercase bg-gray-700 text-gray-400">
+            <tr>
+                <th scope="col" className=" px-2 py-1">
+                    CONTA
+                </th>
+           
+                <th scope="col" className="px-2 py-1">
+                    DESCRIÇÃO
+                </th>
+                <th scope="col" className="px-2 py-1">
+                    TIPO
+                </th> 
+                <th scope="col" className="px-2 py-1">
+                    <span className="sr-only">Edit</span>
+                </th>
+            </tr> 
+        </thead>
+        <tbody>
+            {dados?.plano_contas.map((item,index)=>(
+           <tr key={index}  className={ `border-b bg-gray-800 border-gray-700  hover:bg-gray-600`}>
+            <th scope="row"  className="px-2 py-1 font-medium  whitespace-nowrap">
+                   {item.conta}
+            </th>
+            <td className="px-2 py-1">
+            {item.descricao}
+            </td>   
+            <td className="px-2 py-1">
+               {item.tipo}
+            </td>
+           
+            <td className="px-4 py-1 text-right">
+                <button onClick={(event)=>{
+                               event.stopPropagation() // Garante que o click da linha não se sobreponha ao do botão de Baixar/Editar
+                              }} className="font-medium  text-blue-500 hover:underline">Edit</button>
+            </td>
+           </tr>
+           ))}
 
+            
+           
+        </tbody>
+    
+    </table>
+
+    </div>
+    <div className="flex flex-col rounded-lg  max-h-[calc(100vh-200px)] p-2 max-w-[550px]   shadow-md sm:rounded-lg">
+    <table 
+     className="block  overflow-y-auto overflow-x-auto text-sm text-left rtl:text-center border-collapse rounded-lg text-gray-400">
+        <thead className="sticky top-0  text-xs uppercase bg-gray-700 text-gray-400">
+            <tr>
+                <th scope="col" className=" px-2 py-1">
+                    CONTA
+                </th>
+           
+                <th scope="col" className="px-2 py-1">
+                    DESCRIÇÃO
+                </th>
+                <th scope="col" className="px-2 py-1">
+                    TIPO
+                </th> 
+                <th scope="col" className="px-2 py-1">
+                    <span className="sr-only">Edit</span>
+                </th>
+            </tr> 
+        </thead>
+        <tbody>
+            {dados?.plano_contas.map((item,index)=>(
+           <tr key={index}  className={ `border-b bg-gray-800 border-gray-700  hover:bg-gray-600`}>
+            <th scope="row"  className="px-2 py-1 font-medium  whitespace-nowrap">
+                   {item.conta}
+            </th>
+            <td className="px-2 py-1">
+            {item.descricao}
+            </td>   
+            <td className="px-2 py-1">
+               {item.tipo}
+            </td>
+           
+            <td className="px-4 py-1 text-right">
+                <button onClick={(event)=>{
+                               event.stopPropagation() // Garante que o click da linha não se sobreponha ao do botão de Baixar/Editar
+                              }} className="font-medium  text-blue-500 hover:underline">Edit</button>
+            </td>
+           </tr>
+           ))}
+
+            
+           
+        </tbody>
+    
+    </table>
+
+    </div>
+    </div>
 </div>
 </div>
 </>
