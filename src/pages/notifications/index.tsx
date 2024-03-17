b52b2e248fac70eba832d7a7f6d24f023057f201
HenrickFB <61285599+jhosa12@users.noreply.github.com>
2024-03-16 22:02:34 -0300

    v3

--- src/pages/notifications/index.tsx
+++ src/pages/notifications/index.tsx
@@ -32,8 +32,8 @@ export default function Notificacoes(){
 
     return(
         <>
-        <div className="flex flex-col w-full p-4">
-            <h1 className="flex w-full p-2 border-b-[1px] text-gray-300 font-semibold text-xl border-gray-600">Notificações</h1>
+        <div className="flex flex-col w-full pl-10 pr-10 pt-4">
+            <h1 className="flex w-full p-2 border-b-[1px] text-gray-300 font-semibold text-2xl border-gray-600">Notificações</h1>
             <ul className="flex flex-col gap-3 w-full  p-4">
                 {notificacoes.map((item,index)=>{
                     return(
