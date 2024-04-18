344bf093f6d8ff7a2ccd3000d68f77e281988a6f
HenrickFB <61285599+jhosa12@users.noreply.github.com>
2024-04-18 08:01:53 -0300

    v2

--- src/Documents/convalescenca/comprovante/DocumentTemplate.tsx
+++ src/Documents/convalescenca/comprovante/DocumentTemplate.tsx
@@ -32,56 +32,30 @@ class DocumentTemplate extends React.Component<DadosProps> {
         contrato,
         material } = this.props;
 
-        const options:Intl.DateTimeFormatOptions = {
-            weekday: 'long', // Dia da semana por extenso
-            year: 'numeric', // Ano completo
-            month: 'long', // Mês por extenso
-            day: 'numeric' // Dia do mês
-          };
-          const dataAtual=new Date();
-          const dt = dataAtual.toLocaleDateString('pt-BR', options)
+   
+         
 
     return (
-      <div className='flex flex-col w-full '>
-        <div className="flex w-full justify-center mt-4">
-        <Image className="flex w-40 h-16 mr-7 " src={logo} alt="" />
+      <div className='flex flex-col w-1/2 p-2 border-[1px] '>
+        <div className="flex  w-full justify-center items-center mt-4">
+        <Image className="flex w-1/2 h-16  " src={logo} alt="" />
         </div>
-      
-        <h2 className=' mt-4 text-2xl uppercase text-center font-bold'>Assistência Familiar Paraíso</h2>
-        <p className='text-2xl uppercase text-center font-bold'>FREITAS NETO COMERCIO E SERVIÇOS FUNERARIOS LTDA-ME</p>
-        <p className='text-center'>CNPJ: 16.784.573/0001-93 INSC. EST.:</p>
-        <p className='text-center'>RUA ADALTO CASTELO, 393, CENTRO</p>
-        <p className='text-center'>TELEFONE: (88)99711-3362</p>
-        <h1 className='text-xl text-center font-semibold mt-2'>CONTRATO DE COMPROMISSO</h1>
-        <p className='uppercase mx-5 p-2 text-justify'>
-        OS SIGNATÁRIOS DE INSTRUMENTOS, DE UM LADO, A EMPRESA COM NOME FANTASIA
-ASSISTÊNCIA FAMILIAR PARAÍSO , E RAZÃO SOCIAL FREITAS NETO COMERCIO E
-SERVIÇOS FUNERARIOS LTDA-ME, INSCRITA NO CNPJ 16.784.573/0001-93, ESTABELECIDA A RUA ADALTO CASTELO, 393, CENTRO, CEDRO-CE.
-        </p>
-        <p className='uppercase mx-5 p-2 text-justify'>
-        DE OUTRO LADO, O(A) SR,(A) {nome}, BRASILEIRO(A),
-PORTADOR(A) DO CPF {cpf} E RG {rg}, RESIDENTE E DOMICILIADO A {logradouro}, BAIRRO {bairro},
-CIDADE {cidade}, UF {uf}, TELEFONE {telefone}, CONTRATO Nº {contrato}.<br/>
-        </p>
-        <p className='uppercase mx-5 p-2 text-justify'>
-        Mutualmente convencionam, outorgam e aceitam a saber, as cláusulas e
-condições seguintes:<br/>
-1- O beneficiário, salvo as regras que importem na segurança do uso do
-material LOCADO, obriga-se a devolver o material em boas condições de uso,
-higiene e limpeza como: pintura, proteção plástica, rodas, etiquetas de
-segurança e demais acessórios em perfeito estado de conservação e
-funcionamento, caso contrário o locador terá todo o direito a retenção do
-material LOCADO, ou caso de dano, uma indenização de 50% (cinquenta por
-cento) do valor de venda de mercado do material.<br/><br/>
-2- O objetivo deste contrato destina-se exclusivamente para uso pessoal de
-seus associados e dependentes. Não podendo sua destinação a terceiros. O
-objetivo desta LOCAÇÃO é oferecer fácil locomoção, segurança e conforto para
-os pacientes que fazem uso do mesmo.<br/><br/>
-3- Será cobrado o valor de R$ ___________ mensalmente.<br/><br/>
-Assinam este contrato em duas vias de igual teor.<br/><br/>
-Material: {material}<br/><br/><br/>
-CEDRO-CE, {dt}.
-        </p>
+        <h2 className='text-xl text-center font-semibold mt-2'>COMPROVANTE</h2>
+        <span className="px-2 pt-2" >Data de Devolução:</span>
+        <span className="px-2 pt-2 mx-5">Itens Devolvidos:</span>
+        <ul className="list-item px-2 mx-3 font-semibold">
+          <li className="p-2 mx-2 ">CADEIRA DE RODAS</li>
+
+        </ul>
+        <span className="p-2">Condição do(s) Item(s):</span>
+        <span className="pt-2 px-2 text-center">Assinatura do Recebedor</span>
+        <div className="flex  w-full py-2 justify-center items-center  ">
+          <span className="flex w-10/12 border-b-[1px] p-2 border-black"></span>
+        </div>
+        <span className="p-2">Cedro - {new Date().toLocaleDateString('pt-BR',{month:'long'})}</span>
+        <p className=" mx-5 p-2 text-justify">Este comprovante confirma a devolução dos itens listados acima pelo paciente mencionado
+           na data indicada. Os itens foram verificados quanto à sua condição e quantidade conforme
+            mencionado acima.</p>
       </div>
     );
   }
