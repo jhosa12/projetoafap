b2e22d7a25293d057cf19b66832eee89f8d6baed
HenrickFB <61285599+jhosa12@users.noreply.github.com>
2024-04-24 16:55:18 -0300

    v1

--- src/components/graficos/primeirografico.tsx
+++ src/components/graficos/primeirografico.tsx
@@ -3,11 +3,9 @@ import Chart from  "react-apexcharts";
 
 
 
-class Grafico extends Component {
-  state = {
+export function Grafico()  {
+ const state = {
     options: {
-      
-     
       xaxis: {
         categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
         labels: {
@@ -52,14 +50,14 @@ class Grafico extends Component {
    
   };
 
-  render() {
+  
     return (
       <div className="app">
         <div className="row">
           <div className="mixed-char">
             <Chart
-              options={this.state.options}
-              series={this.state.series}
+              options={state.options}
+              series={state.series}
               type="bar"
               width="600"
               
@@ -68,7 +66,7 @@ class Grafico extends Component {
         </div>
       </div>
     );
-  }
+  
 }
 
 export default Grafico;
