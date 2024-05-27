3b581af3c586508c4cdc58bfb285300836550aac
HenrickFB <61285599+jhosa12@users.noreply.github.com>
2024-05-27 09:48:39 -0300

    b4

--- src/pages/financeiro/login/index.tsx
+++ src/pages/financeiro/login/index.tsx
@@ -15,6 +15,9 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { IoIosAddCircle } from "react-icons/io";
 import { MdSaveAlt } from "react-icons/md";
 import { IoIosClose } from "react-icons/io";
+import { GoArrowDownLeft } from "react-icons/go";
+import { GoArrowUpRight } from "react-icons/go";
+import { MdDelete } from "react-icons/md";
 
 
 interface DataProps {
@@ -387,11 +390,13 @@ export default function LoginFinaceiro() {
           success:'Dados salvos com sucesso'
         }
       )
+      listarContasReq()
     
   }
   async function listarContasReq() {
     const response = await api.get('/conta/listarContas')
     setListaContas(response.data)
+
     
   }
 
@@ -972,30 +977,56 @@ export default function LoginFinaceiro() {
               }
               <button onClick={()=>setModal(true)} className="inline-flex p-1 gap-1 items-center bg-green-600 rounded-lg" ><IoIosAddCircle size={20} /> NOVO</button>
             </div>
-            <div className="grid  gap-2  p-2 w-full grid-cols-4 justify-items-center">
-              {
-                listaContas.map((item,index)=>(
-                  <div key={index} className="max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                  <svg className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z" />
-                  </svg>
-                  <span >
-                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{item.titulo} - {formatter.format(Number(item.valor))}</h5>
-                  </span>
-                  <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{item.descricao}</p>
-                  <span className="inline-flex font-medium items-center text-blue-600 hover:underline">
-                    See our guideline
-                  
-                  </span>
-                </div>
+            <div className="flex flex-col  px-4 w-full overflow-y-auto max-h-[calc(100vh-210px)] text-white bg-[#2b2e3b] rounded-lg ">
+              <ul className="flex flex-col w-full p-2 gap-1 text-sm">
+                <li className="flex flex-col w-full  text-xs pl-4 border-b-[1px] ">
+                  <div className="inline-flex w-full items-center"><span className="flex w-full font-semibold">DESCRIÇÃO</span>
+                    <div className="flex w-full gap-8  items-center">
+                      <span className="flex w-full text-start whitespace-nowrap ">TIPO</span>
+                      <span className="flex w-full text-start whitespace-nowrap">VALOR</span>
+                      <span className="flex w-full text-start whitespace-nowrap ">DATA PREVISTA</span>
+                      <span className="flex w-full text-start whitespace-nowrap justify-end ">AÇÕES</span>
+                     
+                    </div>
+                  </div>
+                </li>
+                {
+                  listaContas?.map((item, index) => {
+                    //   const soma = nome?.lancamentos?.reduce((total, item) => {
+                    //   if (item.conta === nome.conta) {
+                    //     return total + Number(item.valor)
+                    //   }
+                    //   else {
+                    //     return total
+                    //   }
+                    //   }, 0)
+                    // let porc;
+                    //  if (soma === 0 || nome?.metas[0]?.valor === 0 || soma === null || nome?.metas[0]?.valor === null || isNaN(Number(nome?.metas[0]?.valor))) {
+                    //    porc = 0;
+                    //  } else {
+                    //    porc = (soma * 100) / Number(nome?.metas[0].valor);
+                    //  }
 
-                ))
-
-              }
-            
-
-           
-
+                    return (
+                      <li className={`flex flex-col w-full p-1 text-xs pl-4 rounded-lg ${index % 2 === 0 ? "bg-slate-700" : "bg-slate-600"} uppercase cursor-pointer`}>
+                        <div className="inline-flex w-full items-center"><span className="flex w-full font-semibold">{item.descricao}</span>
+                          <div className="flex w-full gap-8  items-center">
+                          <span className="inline-flex w-full text-start whitespace-nowrap"><span className={`inline-flex  rounded-lg ${item.tipo=='PAGAR'?"bg-red-500":"bg-green-500"}   p-1`}>{item.tipo}</span></span>
+                            <span className="flex w-full text-start whitespace-nowrap font-semibold">{formatter.format(Number(item.valor))}</span>
+                            <span className="flex w-full text-start whitespace-nowrap font-semibold">{item.dataprev && new Date(item.dataprev).toLocaleDateString()}</span>
+                         
+                            <div className="flex w-full text-start justify-end whitespace-nowrap">
+                              <button className="rounded-lg text-red-600 hover:bg-gray-300 p-1"><MdDelete size={16}/></button>
+                              </div>
+                           
+                          </div>
+                        </div>
+                      
+                      </li>
+                    )
+                  })
+                }
+              </ul>
             </div>
           </div>
 
