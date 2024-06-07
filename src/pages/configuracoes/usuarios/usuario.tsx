bcf258653cb054427ce94a153be75487aeae9eca
HenrickFB <61285599+jhosa12@users.noreply.github.com>
2024-06-07 12:56:18 -0300

    b4

--- src/pages/configuracoes/usuarios/usuario.tsx
+++ src/pages/configuracoes/usuarios/usuario.tsx
@@ -2,14 +2,19 @@ import { api } from "@/services/apiClient"
 import { getURL } from "next/dist/shared/lib/utils"
 import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
+import fototeste from '../../../../public/fototeste.jpeg'
 
+interface PermissionsProps{
+    button:string
+}
 interface UsersProps{
     cargo:string,
     dir:string,
     email:string,
+    telefone:string
     nome:string,
     id:number,
-    permissoes:JSON,
+    permissoes:Partial<PermissionsProps>,
 }
 
 export default function Usuario() {
@@ -38,14 +43,15 @@ export default function Usuario() {
              <div className="flex flex-col  px-4 w-full overflow-y-auto max-h-[calc(100vh-210px)] text-white bg-[#2b2e3b] rounded-lg ">
               <ul className="flex flex-col w-full p-2 gap-1 text-sm">
                 <li className="flex flex-col w-full  text-xs pl-4 border-b-[1px] ">
-                  <div className="inline-flex w-full items-center"><span className="flex w-full font-semibold">USUÁRIO</span>
-                    <div className="flex w-full gap-8  items-center">
+                  <div className="inline-flex w-full items-center">
+                    <span className="flex w-40 me-2 text-start font-semibold">#</span>
+                    <span className="flex w-full text-start font-semibold">USUÁRIO</span>
                       <span className="flex w-full text-start whitespace-nowrap ">CARGO</span>
-                      <span className="flex w-full text-start whitespace-nowrap">PERMISSÕES</span>
-                      <span className="flex w-full text-start whitespace-nowrap ">META</span>
-                      <span className="flex w-full text-start whitespace-nowrap ">EQV. DE DESPESAS</span>
+                      <span className="flex w-full text-start whitespace-nowrap">E-MAIL</span>
+                      <span className="flex w-full text-start whitespace-nowrap ">TELEFONE</span>
+                      <span className="flex w-full text-start whitespace-nowrap ">PERMISSÕES</span>
                       <span className="flex w-full justify-end  "></span>
-                    </div>
+                   
                   </div>
                 </li>
                 {
@@ -67,14 +73,16 @@ export default function Usuario() {
 
                     return (
                       <li  className={`flex flex-col w-full p-1 text-xs pl-4 rounded-lg ${index % 2 === 0 ? "bg-slate-700" : "bg-slate-600"} uppercase cursor-pointer`}>
-                        <div className="inline-flex w-full items-center"><span className="flex w-full font-semibold">{item?.nome}</span>
-                          <div className="flex w-full gap-8  items-center">
-                            <span className="flex w-full text-start whitespace-nowrap font-semibold">{item.cargo}</span>
-                            <span className="flex w-full text-start whitespace-nowrap font-semibold">{item.permissoes?.button}</span>
-                            <span className="flex w-full text-start whitespace-nowrap"><span className="rounded-lg bg-red-500  p-1">{/*!Number.isNaN(porc) ? porc + '%' : '0%'*/}</span></span>
-                            <span className="flex w-full text-start whitespace-nowrap"><span className="rounded-lg bg-blue-500  p-1">{/*!Number.isNaN(porc) ? porc + '%' : '0%'*/}</span></span>
+                        <div className="inline-flex w-full items-center">
+                            <span className="flex w-40 me-2 text-start font-semibold">  <img className="w-[26px] h-[26px] rounded-full" src="/fototeste.jpeg" alt="Rounded avatar"></img></span>
+                            <span className="flex w-full text-start font-semibold">{item?.nome}</span>
+                            <span className="flex w-full text-start font-semibold">{item.cargo}</span>
+                            <span className="flex w-full text-start font-semibold">{item.email}</span>
+                            <span className="flex w-full text-start ">{item.telefone}</span>
+                            <span className="flex w-full text-start "><span className="rounded-lg bg-blue-500  p-1">{/*!Number.isNaN(porc) ? porc + '%' : '0%'*/}</span></span>
+                            <span className="flex w-full text-end "><span className=" rounded-lg bg-blue-500  p-1">{/*!Number.isNaN(porc) ? porc + '%' : '0%'*/}</span></span>
                            
-                          </div>
+                         
                         </div>
                    
                       </li>
