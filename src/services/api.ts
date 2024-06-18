ba8ef3bcb239fd0810e1ae878bbe78558fb12bad
HenrickFB <61285599+jhosa12@users.noreply.github.com>
2024-06-18 15:02:43 -0300

    x3

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,32 +1,32 @@
-import axios,{AxiosError} from "axios";
+import axios, { AxiosError } from "axios";
 import { parseCookies } from "nookies";
-import {AuthTokenError} from "@/services/errors/AuthTokenError"
+import { AuthTokenError } from "@/services/errors/AuthTokenError"
 import { signOut } from "@/contexts/AuthContext";
 
-export function setupAPIClient(ctx =undefined){
-let cookies =parseCookies(ctx)
+export function setupAPIClient(ctx = undefined) {
+    let cookies = parseCookies(ctx)
 
-const api =axios.create({
-baseURL:process.env.NEXT_PUBLIC_URL_API,
-//baseURL:"http://localhost:3333",
-    headers:{
-        Authorization:`Bearer ${cookies[process.env.NEXT_PUBLIC_COOKIE || '']}`
-    }
-})
-api.interceptors.response.use(response=>{
-    return response;
-},(error:AxiosError)=>{
-    if(error.response?.status===401){
-        //qualquer erro 401 (não autorizado) devemos deslogar o usuario
-        if(typeof window !== undefined){
-            signOut();
-        }else{
-            return Promise.reject(new AuthTokenError())
+    const api = axios.create({
+        baseURL: process.env.NEXT_PUBLIC_URL_API,
+        //baseURL:"http://localhost:3333",
+        headers: {
+            Authorization: `Bearer ${cookies[process.env.NEXT_PUBLIC_COOKIE || '']}`
         }
-    }
-    return Promise.reject(error);
-})
-return api
+    })
+    api.interceptors.response.use(response => {
+        return response;
+    }, (error: AxiosError) => {
+        if (error.response?.status === 401) {
+            //qualquer erro 401 (não autorizado) devemos deslogar o usuario
+            if (typeof window !== undefined) {
+                signOut();
+            } else {
+                return Promise.reject(new AuthTokenError())
+            }
+        }
+        return Promise.reject(error);
+    })
+    return api
 }
 
 
