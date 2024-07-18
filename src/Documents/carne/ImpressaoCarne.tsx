db7acc5d8d15d58b4960e8538f0ec9aaa3ae7d4d
HenrickFB <61285599+jhosa12@users.noreply.github.com>
2024-07-18 09:28:38 -0300

    b5

--- src/Documents/carne/ImpressaoCarne.tsx
+++ src/Documents/carne/ImpressaoCarne.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import logo from "../../../public/logoafap.png"
-import carne from "../../../public/carne.png"
+import carne from "../../../public/carneAtualizado.png"
+import BarCode from 'react-barcode';
 
 // DocumentTemplate.js
 
@@ -53,39 +54,60 @@ class DocumentTemplate extends React.Component<DadosProps> {
 
     render() {
         const { dadosAssociado, arrayMensalidade } = this.props;
-     
+        const formatter = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          });
         return (
-            <ul className="flex flex-col w-full">
+            <ul className="flex flex-col w-full justify-center items-center pt-4">
             {arrayMensalidade.map((item,index)=>(
-               item.status==='A'&& <li key={item.id_mensalidade} style={{borderBottom:'1px dotted',lineHeight: '1'}} className="flex w-full relative text-black text-xs ">
+               item.status==='A'&& <li key={item.id_mensalidade} style={{display:'block',position:'relative',borderBottom:'1px dotted',lineHeight: '1'}} className="flex w-full  text-black text-xs ">
                 
-                    <Image src={carne} className=" object-cover  h-[250px]" alt="modeloCarne" />
+                    <Image src={carne} className=" object-cover  h-[250px]" alt="modeloCarne"  />
                
-                <h1 style={{ position: 'absolute', left: "35px", top: '9px' }} >ASSISTENCIA FAMILIAR PARAÍSO</h1>
-                <h1 style={{ position: 'absolute', left: "250px", top: '9px', fontWeight: 'bold' }} >ASSISTENCIA FAMILIAR PARAÍSO</h1>
-                <span style={{ position: 'absolute', left: '70px', top: '42px' }}>{item.parcela_n}</span>
-                <span style={{ position: 'absolute', left: '140px', top: '42px', fontWeight: 'bold' }}>{item.vencimento && new Date(item.vencimento).toLocaleDateString('pt-BR',{timeZone:'UTC'})}</span>
-                <span style={{ position: 'absolute', left: '140px', top: '75px', fontWeight: 'bold' }}>{item.valor_total}</span>
-                <span style={{ position: 'absolute', left: '140px', top: '104px' }}>{item.valor_total}</span>
-                <span style={{ position: 'absolute', left: '140px', top: '140px' }}>{new Date().toLocaleDateString('pt-BR')}</span>
-                <div style={{ position: 'absolute', left: '32px', top: '172px', width: '180px' }} className="flex flex-col "  >
+                <h1 style={{fontSize:'13px', position: 'absolute', left: "45px", top: '19px' }} >ASSISTENCIA FAMILIAR PARAÍSO</h1>
+                <h1 style={{fontSize:'13px', position: 'absolute', left: "330px", top: '19px', fontWeight: 'bold' }} >ASSISTENCIA FAMILIAR PARAÍSO</h1>
+                <span style={{ position: 'absolute', left: '100px', top: '62px' }}>{item.parcela_n}</span>
+                <span style={{ position: 'absolute', left: '170px', top: '62px', fontWeight: 'bold' }}>{item.vencimento && new Date(item.vencimento).toLocaleDateString('pt-BR',{timeZone:'UTC'})}</span>
+                <span style={{ position: 'absolute', left: '170px', top: '105px', fontWeight: 'bold' }}>{formatter.format(Number(item.valor_principal))}</span>
+                {/*<span style={{ position: 'absolute', left: '140px', top: '104px' }}>{item.valor_principal}</span>*/}
+                <span style={{ position: 'absolute', left: '170px', top: '190px' }}>{new Date().toLocaleDateString('pt-BR')}</span>
+                <div style={{ position: 'absolute', left: '45px', top: '230px', width: '180px' }} className="flex flex-col "  >
                     <span style={{ fontSize: '9px', fontWeight: 'bold', margin: '0', padding: '0', lineHeight: '1' }}>{dadosAssociado.nome}</span>
                     <span style={{ fontSize: '8px', margin: '0', padding: '0', lineHeight: '1' }}>{dadosAssociado.endereco}- {dadosAssociado.numero}</span>
                     <span style={{ fontSize: '8px', margin: '0', padding: '0', lineHeight: '1' }}>{dadosAssociado.bairro}-{dadosAssociado.cidade}/{dadosAssociado.uf}</span>
                 </div>
-                <span style={{ position: 'absolute', left: '250px', top: '42px' }}>RUA ADAUTO CASTELO - 393 CENTRO CEDRO/CE</span>
+                <span style={{ position: 'absolute', left: '330px', top: '62px' }}>RUA ADAUTO CASTELO - 393 CENTRO CEDRO/CE</span>
 
-                <span style={{ position: 'absolute', left: '620px', top: '42px', fontWeight: 'bold' }}>{item.vencimento && new Date(item.vencimento).toLocaleDateString('pt-BR',{timeZone:'UTC'})}</span> {/*VENCIMENTO*/}
-                <span style={{ position: 'absolute', left: '720px', top: '42px' }}>{item.parcela_n}</span> {/*N PARCELA*/}
-                <span style={{ position: 'absolute', left: '705px', top: '72px', fontWeight: 'bold' }}>{item.valor_total}</span> {/*Valor doc*/}
-                <span style={{ position: 'absolute', left: '250px', top: '72px' }}>16.784.573/0001-93 - ASSITÊNCIA FAMILIAR PARAÍSO</span> {/*CEDENTE*/}
-                <span style={{ position: 'absolute', left: '250px', top: '102px' }}>{new Date().toLocaleDateString('pt-BR')}</span> {/*DATA DOC*/}
-                <span style={{ position: 'absolute', left: '380px', top: '102px' }}>{dadosAssociado.id_contrato}</span> {/*CONTRATO*/}
-                <span style={{ position: 'absolute', left: '470px', top: '102px' }}>{dadosAssociado.plano}</span> {/*CONTRATO*/}
-                <div style={{ position: 'absolute', left: '250px', top: '185px' }} className="flex flex-col "  >
+                <span style={{ position: 'absolute', left: '810px', top: '62px', fontWeight: 'bold' }}>{item.vencimento && new Date(item.vencimento).toLocaleDateString('pt-BR',{timeZone:'UTC'})}</span> {/*VENCIMENTO*/}
+                <span style={{ position: 'absolute', left: '940px', top: '62px' }}>{item.parcela_n}</span> {/*N PARCELA*/}
+                <span style={{ position: 'absolute', left: '940px', top: '102px', fontWeight: 'bold' }}>{formatter.format(Number(item.valor_principal))}</span> {/*Valor doc*/}
+                <span style={{ position: 'absolute', left: '330px', top: '102px' }}>16.784.573/0001-93 - ASSITÊNCIA FAMILIAR PARAÍSO</span> {/*CEDENTE*/}
+                <span style={{ position: 'absolute', left: '360px', top: '142px' }}>{new Date().toLocaleDateString('pt-BR')}</span> {/*DATA DOC*/}
+                <span style={{ position: 'absolute', left: '490px', top: '142px' }}>{dadosAssociado.id_contrato}</span> {/*CONTRATO*/}
+                <span style={{ position: 'absolute', left: '600px', top: '142px' }}>{dadosAssociado.plano}</span> {/*CONTRATO*/}
+                <span style={{ position: 'absolute', left: '150px', top: '315px',fontSize:'14px',fontWeight:'bold' }}>{dadosAssociado.id_contrato}</span>
+                <div style={{ position: 'absolute', left: '330px', top: '235px' }} className="flex flex-col "  >
                     <span style={{ fontSize: '12px', fontWeight: 'bold', margin: '0', padding: '0', lineHeight: '1' }}>{dadosAssociado.nome}</span>
                     <span style={{ fontSize: '9px', margin: '0', padding: '0', lineHeight: '1' }}>{dadosAssociado.endereco}-{dadosAssociado.numero}</span>
                     <span style={{ fontSize: '9px', margin: '0', padding: '0', lineHeight: '1' }}>{dadosAssociado.bairro}-{dadosAssociado.cidade}/{dadosAssociado.uf}</span>
+                </div>
+                <div  style={{display:'flex', flexDirection:'column',position: 'absolute', left: '330px', top: '182px',zIndex:2 }}>
+                <span style={{ fontSize: '12px', fontWeight: 'bold', margin: '0', padding: '0', lineHeight: '1' }}>PIX: 16.784.573/0001-93</span>
+                    <span style={{ fontSize: '9px', margin: '0', padding: '0', lineHeight: '1' }}>TITULAR FREITAS NETO COMERCIO E SERVIÇO- LTDA-ME</span>
+                    <span style={{ fontSize: '9px', margin: '0', padding: '0', lineHeight: '1',fontStyle:'italic' }}>MANTENHA SEU PLANO DE EM DIA</span>
+                </div>
+               
+                <div style={{position: 'absolute', left: '720px', top: '290px',width:'300px',height:'30px',zIndex:2 }}>
+                    {item.n_doc && <BarCode
+                     width={3}
+                      height={30} 
+                      fontSize={10}
+                      textMargin={0}
+                      margin={0}
+                    
+                      value={item.n_doc}/>}
+                  
                 </div>
             </li>)
     )}
