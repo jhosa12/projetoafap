e3297e2d566df982fb508d04064c6b04abb0ce90
jhosa12 <henriquefreitas903@gmail.com>
2025-10-14 10:20:00 -0300

    refactor:ajuste temporario

--- src/app/dashboard/sorteios/configuracoes/page.tsx
+++ src/app/dashboard/sorteios/configuracoes/page.tsx
@@ -1,41 +1,48 @@
 'use client';
 
 
-import CadastroPremio from "../_config/cadastroPremio"
-import ConsultarGanhadores from "../_config/consultar"
-import { Tabs } from "flowbite-react"
-import { PiListBulletsFill } from "react-icons/pi"
-import { GiPodiumWinner } from "react-icons/gi"
+// import CadastroPremio from "../_config/cadastroPremio"
+// import ConsultarGanhadores from "../_config/consultar"
+// import { Tabs } from "flowbite-react"
+// import { PiListBulletsFill } from "react-icons/pi"
+// import { GiPodiumWinner } from "react-icons/gi"
 
 
-export default function ConfigSort() {
+// export default function ConfigSort() {
 
-  return (
-    <div className="flex flex-col  w-full text-white">
-      <Tabs theme={{
-        base: 'bg-white rounded-b-lg', tabpanel: 'bg-white rounded-b-lg h-[calc(100vh-105px)]', tablist: {
-          tabitem: {
-            base: "flex items-center  justify-center rounded-t-lg px-4 py-3 text-sm font-medium first:ml-0  disabled:cursor-not-allowed disabled:text-gray-400 disabled:dark:text-gray-500", variant: {
-              underline: {
-                active: {
-                  on: "active rounded-t-lg border-b-2 border-blue-600 text-blue-500 ",
-                  off: "border-b-2 border-transparent text-black hover:border-gray-700 hover:text-gray-600 "
-                }
-              }
-            }
-          }
-        }
-      }} variant="underline">
-        <Tabs.Item active title="Prêmios" icon={PiListBulletsFill}>
-          <CadastroPremio />
-        </Tabs.Item>
-        <Tabs.Item title="Ganhadores" icon={GiPodiumWinner}>
-          <ConsultarGanhadores />
-        </Tabs.Item>
-      </Tabs>
-    </div>
+//   return (
+//     <div className="flex flex-col  w-full text-white">
+//       <Tabs theme={{
+//         base: 'bg-white rounded-b-lg', tabpanel: 'bg-white rounded-b-lg h-[calc(100vh-105px)]', tablist: {
+//           tabitem: {
+//             base: "flex items-center  justify-center rounded-t-lg px-4 py-3 text-sm font-medium first:ml-0  disabled:cursor-not-allowed disabled:text-gray-400 disabled:dark:text-gray-500", variant: {
+//               underline: {
+//                 active: {
+//                   on: "active rounded-t-lg border-b-2 border-blue-600 text-blue-500 ",
+//                   off: "border-b-2 border-transparent text-black hover:border-gray-700 hover:text-gray-600 "
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }} variant="underline">
+//         <Tabs.Item active title="Prêmios" icon={PiListBulletsFill}>
+//           <CadastroPremio />
+//         </Tabs.Item>
+//         <Tabs.Item title="Ganhadores" icon={GiPodiumWinner}>
+//           <ConsultarGanhadores />
+//         </Tabs.Item>
+//       </Tabs>
+//     </div>
 
+//   )
+
+// }
+
+
+export default function Page(){
+  return(
+    <div></div>
   )
-
 }
 
